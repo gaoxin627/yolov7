258e3078d9576bc10bd172cc01e16e9be6b243e6
gaoxin627 <gaoxin627@sina.com>
2023-04-20 14:33:28 +0800

    修改最佳模型保存策略，以mAP0.5为指标

--- utils/metrics.py
+++ utils/metrics.py
@@ -12,6 +12,7 @@ from . import general
 def fitness(x):
     # Model fitness as a weighted combination of metrics
     w = [0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
+    w = [0.0, 0.0, 1.0, 0.0]
     return (x[:, :4] * w).sum(1)
 
 
